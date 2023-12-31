4d46d72280a35ece8e087beddce0bebcf86abc18
minji0708 <155282552+minji0708@users.noreply.github.com>
2023-12-31 13:46:06 +0900

    Add files via upload

---
+++ src_game.py
@@ -0,0 +1,36 @@
+import random
+lb_comp = Element("lb_comp")
+
+def scissor(*args):
+    
+    #=======================
+
+    rand()
+
+    #=======================
+
+def rock(*args):
+
+    #=======================
+
+    rand()
+
+    #=======================
+
+def paper(*args):
+   
+    #=======================
+
+    rand()
+
+    #=======================
+def rand(*args):
+
+    #=======================
+
+    a = random.randrange(0,3)
+    b = ["가위","바위","보"]
+
+    #=======================
+
+    lb_comp.element.innerText = b[a]
